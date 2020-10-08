2dcc33c41852ddcdb9002b8b53aab4e7800b576f
Diego Lima <diego-dlp@hotmail.com>
2020-10-08 07:18:17 -0300

    Modificações Importantes

--- enter-code/comando.js
+++ enter-code/comando.js
@@ -1,10 +1,12 @@
-var i = 600;
-if (i % 2 == 0) {
-  console.log("É número par!");
-}
-else {
+// var i = 600;
+// if (i % 2 == 0) {
+//   console.log("É número par!");
+// }
+// else {
   console.log("É número ímpar!");
-}
-for (j = 0; j < 1; j++) {
-  console.log(j);
-}
+// }
+// for (j = 0; j < 1; j++) {
+//   console.log(j);
+// }
+
+var list = [1,2,3];
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var fs = require('file-system')
+
 var operators = require('./language/operators')
 var reservedWords = require('./language/reservedWords')
 var terminators = require('./language/terminators')
@@ -6,151 +7,157 @@ var terminators = require('./language/terminators')
 var code = fs.readFileSync('enter-code/comando.js', 'utf8')
 var codeLines = code.split('\n');
 
-var saida_arquivo = 'Token; Lexema; Descrição; Linha do Código; Posição na Linha;\n';
-var aux_token_repetido = [];
+var cvsFile = 'Token; Lexema; Descrição; Linha do Código; Posição na Linha;\n';
 
+var trail = "nada";             // Identifica o tipo de char anterior ao atual (identificar sequencias)
+var current_token = ''; 
+var comment = false;
+var open_quotes = false;              // Se for true todo o conteúdo deve ser salvo como um só
 
+var cont_line = 0;
+var cont_column = 0;
 
-var token_atual = '';                   // Armazena a sequencia de caracteres que forma um token
-var pr_encontrados = [];
-var identificadores_encontrados = [];
-var operadores_encontrados = [];
-var constantes_encontrados = [];        // Somente números inteiros
-var literais_encontrados = [];
-var terminadores_encontrados = [];
-
-var aspas_abertas = false;              // Se for true todo o conteúdo deve ser salvo como um só
-var rastro = "nada";                    // Identifica o tipo de caractere anterior ao atual (identificar sequencias)
-var cont_linha = 0;                     // Identifica a linha de um token
-var cont_coluna = 0;
-
-
-for (caractere of code) {
-  if (aspas_abertas) {
-    if (caractere == '"') {
-      aspas_abertas = !aspas_abertas;
-      rastro = "literal";
-      token_atual = checar_token(token_atual);
+for (char of code) {
+  if (open_quotes) {
+    current_token += char;
+    
+    if (char == '"') {
+      open_quotes = !open_quotes;
+      trail = "literal";
+      current_token = checkToken(current_token);
     }
-    token_atual += caractere;
-    rastro = "terminador";
-  }
 
-  else if (caractere == "\n") {
-    cont_linha++;
-    cont_coluna = 0;
-  }
+    trail = "terminador";
 
-  else {
-    if (!eh_espaco(caractere)) {
-      if (!eh_operador(caractere)) {
-        if (!eh_terminador(caractere)) {
-          if (!eh_numero(caractere)) {
-            if (rastro != "letra") {
-              token_atual = checar_token(token_atual);
-            }
-            token_atual += caractere;
-            rastro = "letra";
+  } else if (comment) {
+    if (char == "\n") {
+      comment = false;
+      current_token = checkToken(current_token);
+    } else {
+      current_token += char;
+    }
+
+    trail = "comentario";
+
+  } else if (char == "\n") {
+    cont_line++;
+    cont_column = 0;
+
+  } else {
+    if (!is_space(char)) {
+
+      if (!is_operator(char)) {
+
+        if (!is_terminator(char)) {
+
+          if (!is_number(char)) {
+            if (trail != "letra")
+              current_token = checkToken(current_token);
+
+            current_token += char;
+            trail = "letra";
+          } else if (Number.isInteger(parseInt(current_token[0]))) {
+            current_token += char;
+            trail = "constante";
+          } else {
+            current_token = checkToken(current_token);
+            current_token += char;
+            trail = "constante";
           }
-          else if (Number.isInteger(parseInt(token_atual[0]))) {
-            token_atual += caractere;
-            rastro = "constante";
+
+        } else {
+          if (char == '"') {
+            current_token = '"';
+            open_quotes = !open_quotes;
+          } else {
+            current_token = checkToken(current_token);
+            current_token += char;
+            trail = "terminador";
           }
-          else {
-            token_atual = checar_token(token_atual);
-            token_atual += caractere;
-            rastro = "constante";
-          }
+
         }
-        else {
-          token_atual = checar_token(token_atual);
-          token_atual += caractere;
-          rastro = "terminador";
-          if (caractere == '"') {
-            aspas_abertas = !aspas_abertas;
-            token_atual = checar_token(token_atual);
-          }
-        }
+
+      } else if (operators.includes(current_token[0])) {
+        current_token += char;
+        trail = "operador";
+        if(is_comment())
+          comment = true;
+
+      } else {
+        current_token = checkToken(current_token);
+        current_token += char;
+        trail = "operador";
+
       }
-      else if (operators.includes(token_atual[0])) {
-        token_atual += caractere;
-        rastro = "operador";
-      }
-      else {
-        token_atual = checar_token(token_atual);
-        token_atual += caractere;
-        rastro = "operador";
-      }
+
+    } else {
+      current_token = checkToken(current_token);
+      trail = "nada";
     }
-    else {
-      token_atual = checar_token(token_atual);
-      rastro = "nada";
-    }
+
   }
-  cont_coluna++;
+
+  cont_column++;
+
 }
 
+fs.writeFile('./output/resultado.csv', cvsFile, function (err) { })
 
-// concat_retorno_arquivo(pr_encontrados, 'Palavra Reservada');
-// concat_retorno_arquivo(identificadores_encontrados, 'Identificador');
-// concat_retorno_arquivo(operadores_encontrados, 'Operador');
-// concat_retorno_arquivo(literais_encontrados, 'Literal');
-// concat_retorno_arquivo(constantes_encontrados, 'Constante');
+function checkToken(current_token) {
+  if (current_token == ',') {
+    return '';
 
-fs.writeFile('./output/resultado.csv', saida_arquivo, function (err) { })
-
-
-
-
-
-
-function checar_token(token_atual) {
-  if (token_atual != '') {
-    switch (rastro) {
+  } else if (current_token != '') {
+    switch (trail) {
       case "nada":
         break;
+
+      case "comentario":
+        insertTable(current_token, 'Comentário');
+        break;
+
       case "letra":
-        if (reservedWords.includes(token_atual)) {
-          insertTable(token_atual, 'Palavra Reservada');
-          pr_encontrados.push(token_atual);
-        }
-        else if (token_atual != "\n") {
-          insertTable(token_atual, 'Identificador');
-          identificadores_encontrados.push(token_atual);
+        if (reservedWords.includes(current_token)) {
+          insertTable(current_token, 'Palavra Reservada');
+        } else if (current_token != "\n") {
+          insertTable(current_token, 'Identificador');
         }
         break;
+
       case "operador":
-        insertTable(token_atual, 'Operador');
-        operadores_encontrados.push(token_atual);
+        insertTable(current_token, 'Operador');
         break;
+
       case "constante":
-        insertTable(token_atual, 'Constante');
-        constantes_encontrados.push(token_atual);
+        insertTable(current_token, 'Constante');
         break;
+
       case "literal":
-        insertTable(token_atual, 'Literal');
-        literais_encontrados.push(token_atual);
+        insertTable(current_token, 'Literal');
         break;
+
       case "terminador":
-        insertTable(token_atual, 'Terminador');
-        terminadores_encontrados.push(token_atual);
+        insertTable(current_token, 'Terminador');
         break;
+
     }
   }
+
   return '';
 }
 
-
-
-
-
-function insertTable(id, tipo_token) {
+function insertTable(id, token_type) {
   var token = "";
-  switch(tipo_token) {
+  switch(token_type) {
     case 'Identificador':
     case 'Constante':
     case 'Literal':
-      token = `<${tipo_token}, ${id}>`;
+      token = `<${token_type}, ${id}>`;
+      break;
+
+    case 'Comentário':
+      token = `<${token_type}, ${id}>`;
+      id = '//';
       break;
 
     default:
@@ -158,93 +165,48 @@ function insertTable(id, tipo_token) {
       break;
   }
 
-  linha_e_coluna = get_linha_e_coluna_token(id);
-  saida_arquivo += `${token}; ${id}; ${tipo_token}; ${linha_e_coluna.linha}; ${linha_e_coluna.coluna}\n`;
+  line_column = get_line_column_token(id);
+  cvsFile += `${token}; ${id}; ${token_type}; ${line_column.line}; ${line_column.column}\n`;
 
-  // if (aux_token_repetido[id]) {
-  //   ultima_col = aux_token_repetido[id].ultima_col + 1
-  //   ultima_linha = aux_token_repetido[id].ultima_linha
-
-  //   linha_e_coluna = get_linha_e_coluna_token(id, ultima_col, ultima_linha);
-
-  //   saida_arquivo += `${token}; ${id}; ${tipo_token}; ${linha_e_coluna.linha}; ${linha_e_coluna.coluna}\n`;
-
-  //   aux_token_repetido[id] = { token: id, qtd: aux_token_repetido[id].qtd + 1, ultima_col: linha_e_coluna.coluna, ultima_linha: linha_e_coluna.linha }
-  // } else {
-  //   linha_e_coluna = get_linha_e_coluna_token(id);
-  //   saida_arquivo += `${token}; ${id}; ${tipo_token}; ${linha_e_coluna.linha}; ${linha_e_coluna.coluna}\n`;
-
-  //   aux_token_repetido[id] = { token: id, qtd: 1, ultima_col: linha_e_coluna.coluna, ultima_linha: linha_e_coluna.linha }
-  // }
-  // aux_token_repetido = []
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-function eh_espaco(caractere) {
-  return caractere === ' ';
+function is_space(char) {
+  return char === ' ';
 }
 
-function eh_operador(caractere) {
-  return operators.includes(caractere);
+function is_comment() {
+  return current_token === '//';
 }
 
-function eh_terminador(caractere) {
-  return terminators.includes(caractere)
+function is_operator(char) {
+  return operators.includes(char);
 }
 
-function eh_numero(caractere) {
-  var n = parseInt(caractere);
+function is_terminator(char) {
+  return terminators.includes(char)
+}
+
+function is_number(char) {
+  var n = parseInt(char);
   return Number.isInteger(n);
 }
 
-// function concat_retorno_arquivo(items, tipo_token) {
-//   for (id of items) {
-//     if (aux_token_repetido[id]) {
-//       ultima_col = aux_token_repetido[id].ultima_col + 1
-//       ultima_linha = aux_token_repetido[id].ultima_linha
-
-//       linha_e_coluna = get_linha_e_coluna_token(id, ultima_col, ultima_linha);
-
-//       saida_arquivo += `${id}; ${tipo_token}; ${linha_e_coluna.linha};${linha_e_coluna.coluna}\n`;
-
-//       aux_token_repetido[id] = { token: id, qtd: aux_token_repetido[id].qtd + 1, ultima_col: linha_e_coluna.coluna, ultima_linha: linha_e_coluna.linha }
-//     } else {
-//       linha_e_coluna = get_linha_e_coluna_token(id);
-//       saida_arquivo += `${id}; ${tipo_token}; ${linha_e_coluna.linha};${linha_e_coluna.coluna}\n`;
-
-//       aux_token_repetido[id] = { token: id, qtd: 1, ultima_col: linha_e_coluna.coluna, ultima_linha: linha_e_coluna.linha }
-//     }
-//   }
-//   aux_token_repetido = []
-// }
-
-function get_linha_e_coluna_token(token, ultima_col = false, ultima_linha = false) {
-  for (const linha of codeLines) {
-    var col_token = linha.indexOf(token);
+function get_line_column_token(token, ultima_col = false, ultima_line = false) {
+  for (const line of codeLines) {
+    var col_token = line.indexOf(token);
 
     if (col_token >= 0) {
-      var linha_token = codeLines.indexOf(linha)
+      var line_token = codeLines.indexOf(line)
 
-      if (!(ultima_col && ultima_linha)) {
-        return { linha: linha_token + 1, coluna: col_token + 1 };
+      if (!(ultima_col && ultima_line)) {
+        return { line: line_token + 1, column: col_token + 1 };
 
       } else {
-        if (linha_token == ultima_linha && col_token > ultima_col) {
-          return { linha: linha_token + 1, coluna: col_token + 1 };
+        if (line_token == ultima_line && col_token > ultima_col) {
+          return { line: line_token + 1, column: col_token + 1 };
 
-        } else if ((linha_token != ultima_linha || col_token != ultima_col) && (linha_token > ultima_linha || col_token > ultima_col)) {
-          return { linha: linha_token + 1, coluna: col_token + 1 };
+        } else if ((line_token != ultima_line || col_token != ultima_col) && (line_token > ultima_line || col_token > ultima_col)) {
+          return { line: line_token + 1, column: col_token + 1 };
 
         }
       }
